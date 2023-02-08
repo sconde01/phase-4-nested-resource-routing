055534e22c24b46af2f816c7fdcf8ab1b3ec5286
Stephanie Conde <stephaniecondecareer@gmail.com>
2023-02-07 18:06:12 -0800

    commit tree 286631d68603c1fadbb7e495728fed10fbcb418a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
+  
+  resources :dog_houses, only: [:show] do
+    #nested resource for reviews
+    resources :reviews, only: [:show, :index]
+  end
+    #Now we have the resourced :dog_houses route, but by adding the do...end we can pass it a block of its nested routes.
+
+
   resources :reviews, only: [:show, :index, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
